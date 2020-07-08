5f4d739cb196271ad9c65bbcfabc5446072e56d5
xetSumit <xettrisumit@gmail.com>
2020-07-08 11:03:40 +0545

    ch_4done(listview,listTile,http json data fetch)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:lets_begin/change_name_card.dart';
-import 'package:lets_begin/drawer.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:lets_begin/drawer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,25 +12,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
   var _text = "change me";
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+  getData() async{
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {
+      
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[400],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Lets Go 2020!"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: ChangeNameCard(text: _text, nameController: _nameController),
-          ),
-        ),
+        child: data != null
+            ? ListView.builder(
+              itemBuilder: (context,index){
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title :Text(data[index]["title"]),
+                    leading: Image.network(data[index]["url"]), 
+                    subtitle: Text("ID : ${data[index]["id"]}"),
+                  ),
+                );
+              },
+              itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.blueGrey,
+                ),
+              ),
       ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
@@ -42,4 +68,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
