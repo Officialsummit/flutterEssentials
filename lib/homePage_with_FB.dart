b1e8277c2f3892ccbee060c121bd75d6a4fd9e3e
xetSumit <xettrisumit@gmail.com>
2020-07-11 10:45:30 +0545

    Future builder implementation

--- lib/homePage_with_FB.dart
+++ lib/homePage_with_FB.dart
@@ -4,24 +4,13 @@ import 'package:http/http.dart' as http;
 import 'package:lets_begin/util/Constants.dart';
 import 'package:lets_begin/widgets/drawer.dart';
 
-class HomePageWithFb extends StatefulWidget {
-  @override
-  _HomePageWithFbState createState() => _HomePageWithFbState();
-}
-
-class _HomePageWithFbState extends State<HomePageWithFb> {
-  var url = "https://jsonplaceholder.typicode.com/photos";
-  var data;
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
+class HomePageWithFb extends StatelessWidget {
   Future getData() async {
+    var url = "https://jsonplaceholder.typicode.com/photos";
+    var data;
     var res = await http.get(url);
     data = jsonDecode(res.body);
-    setState(() {});
+    return data;
   }
 
   @override
@@ -41,43 +30,43 @@ class _HomePageWithFbState extends State<HomePageWithFb> {
       ),
       body: FutureBuilder(
         future: getData(),
-        builder: (context,snapshot) {
-          switch(snapshot.connectionState){
-            case ConnectionState.none:   
-            return Center(
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Center(
                 child: Text("Please fetch some data"),
               );
             case ConnectionState.active:
             case ConnectionState.waiting:
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-            case ConnectionState.done:
-            if(snapshot.hasError){
-              return Center(child: Text("Oops! something unsual happened"),);
-            }
-            return ListView.builder(
-              itemBuilder: (context,index){
-                return Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ListTile(
-                    title: Text(snapshot.data[index]["title"]),
-                    subtitle: Text("ID:${snapshot.data[index]["id"]}"),
-                    leading: Image.network(snapshot.data[index]["url"]),
-                  ),
-                );
-                
-              },
-              itemCount: snapshot.data.length,
+              return Center(
+                child: CircularProgressIndicator(),
               );
-
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text("Oops! something unsual happened"),
+                );
+              }
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ListTile(
+                      title: Text(snapshot.data[index]["title"]),
+                      subtitle: Text("ID:${snapshot.data[index]["id"]}"),
+                      leading: Image.network(snapshot.data[index]["url"]),
+                    ),
+                  );
+                },
+                itemCount: snapshot.data.length,
+              );
           }
         },
       ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {});
+          
         },
         child: Icon(Icons.refresh),
       ),
